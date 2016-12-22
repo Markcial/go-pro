80895e6cf7806042906bc360ab282c3fba87c18f
Marc Garcia Sastre <markcial@gmail.com>
2016-12-22 08:29:55 +0100

    Adds template rendering to webserver example

--- 10-webserver/main.go
+++ 10-webserver/main.go
@@ -3,10 +3,26 @@ package main
 import (
   "fmt"
   "net/http"
+  "html/template"
 )
 
+type WebPage struct {
+  Title string
+  Header string
+  Table [][]string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  p := &WebPage{
+    Title: "My Webpage",
+    Header: "Webpage with table data",
+    Table: [][]string{
+      []string{"a", "row", "of", "data"},
+      []string{"another", "row", "of", "data"},
+    },
+  }
+  t, _ := template.ParseFiles("./10-webserver/index.html")
+  t.Execute(w, p)
 }
 
 func main() {
